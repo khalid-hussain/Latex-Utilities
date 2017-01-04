cc8c08f593a854d9ec01be188f3453ca0a8b6409
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2017-01-05 07:33:44 +0800

    Refactored everything

--- KFGQPC_Symbols/nass_and_symbol_to_latex.py
+++ KFGQPC_Symbols/nass_and_symbol_to_latex.py
@@ -2,13 +2,13 @@
 import linecache
 
 f_nusoos = 'symbol_nass_list.txt'
-f_keys   = 'symbol_key_list.txt'
+f_keys = 'symbol_key_list.txt'
 contentfile = open('content.tex', 'w+', encoding="utf-8")
 
 theLine = " "
 
 for x in range(96):
-  contentfile.write('{} & {{\QPCSymbols\XeTeXglyph {}}}  & \\textarabic{{{}}} & \\texttt{{{}}} & \\verb$\XeTeXglyph {}$  \\\\'.format(x+1, x+2,linecache.getline(f_nusoos, x+1).rstrip('\n'), linecache.getline(f_keys, x+1).rstrip('\n'), x+2))
-  contentfile.write('\n')
-  contentfile.write('\hline')
-  contentfile.write('\n')
+    contentfile.write('{} & {{\QPCSymbols\XeTeXglyph {}}}  & \\textarabic{{{}}} & \\texttt{{{}}} & \\verb$\XeTeXglyph {}$  \\\\'.format(x + 1, x + 2, linecache.getline(f_nusoos, x + 1).rstrip('\n'), linecache.getline(f_keys, x + 1).rstrip('\n'), x + 2))
+    contentfile.write('\n')
+    contentfile.write('\hline')
+    contentfile.write('\n')
